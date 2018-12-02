6faa20fb1b03050f7edb8115b86dc55f892904c5
NadeenAhmad <36262947+NadeenAhmad@users.noreply.github.com>
2018-12-02 12:49:17 -0800

    Update Database Schema (tasks)

--- models/Item.js
+++ models/Item.js
@@ -1,1 +1,15 @@
-test
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Item = mongoose.model('item', ItemSchema);
